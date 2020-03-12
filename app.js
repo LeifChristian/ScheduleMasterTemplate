1a1c4e72e18e10a942fefbfd4e75aaf617b28991
AirbenderProductions <mtmusicandart@gmail.com>
2020-03-12 09:33:36 -0600

    working frontend, added CORS code

--- app.js
+++ app.js
@@ -40,6 +40,15 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "./client-react/build/index.html"));
   });
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+
   console.log('latest commit working!');
 
 
